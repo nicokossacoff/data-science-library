66ebdb2b513e8cfea4d4a58e6ea4ff52e39eab2f
nicokossacoff <nicokossacoff@gmail.com>
2025-05-25 16:13:09 -0300

    Updated PyTorch workflow notebooks

---
+++ coding/py-torch/00-fundamentals/01-workflow/config.py
@@ -0,0 +1,9 @@
+import torch
+class Config:
+    def __init__(self):
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+        else:
+            self.device = torch.device("cpu")
