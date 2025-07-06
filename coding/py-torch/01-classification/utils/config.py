137c36808081dc14c3256e1dc72d90d5f11385fd
nicokossacoff <nicokossacoff@gmail.com>
2025-07-05 23:13:48 -0300

    Updated the config class

--- coding/py-torch/01-classification/utils/config.py
+++ coding/py-torch/01-classification/utils/config.py
@@ -3,12 +3,24 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Config:
-    def __init__(self):
+    """
+    Configuration class for managing device and hyperparameters for PyTorch neural network training.
+    This class automatically detects the available computation device (CUDA, MPS, or CPU) and dynamically sets attributes for each hyperparameter provided in the input dictionary.
+    
+    Args:
+        hyperparameters (dict): A dictionary containing hyperparameter names and their corresponding values.
+    """
+
+    def __init__(self, hyperparameters: dict):
         if torch.cuda.is_available():
             self.device = torch.device("cuda")
         elif torch.backends.mps.is_available():
             self.device = torch.device("mps")
         else:
             self.device = torch.device("cpu")
+
+        # Generate attributes from every hyperparameter
+        for (key, value) in hyperparameters.items():
+            setattr(self, key, value)
         
         logging.info(f"Current device: {self.device}")
