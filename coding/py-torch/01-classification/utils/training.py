b1768d6ec373bb878d31ab87c1e98b714373653f
nicokossacoff <nicokossacoff@gmail.com>
2025-07-27 18:44:35 -0300

    Updated the validation functions

--- coding/py-torch/01-classification/utils/training.py
+++ coding/py-torch/01-classification/utils/training.py
@@ -1,9 +1,13 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from utils.metrics import *
+import sklearn.metrics as metrics
 
-def __convert_scores_to_probabilities(scores: torch.Tensor) -> torch.Tensor:
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def convert_scores_to_probabilities(scores: torch.Tensor) -> torch.Tensor:
     """
     Converts model scores to probabilities based on the number of output classes.
 
@@ -18,7 +22,7 @@ def __convert_scores_to_probabilities(scores: torch.Tensor) -> torch.Tensor:
     else:
         return torch.softmax(scores, dim=1)
 
-def validation_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, criterion: torch.nn.Module, device: torch.device) -> float:
+def binary_validation_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, criterion: torch.nn.Module, device: torch.device, ths: float = 0.5) -> float:
     """
     Performs a validation step on the model using the provided dataloader.
 
@@ -55,7 +59,87 @@ def validation_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoa
             # Convert scores to probabilities
             # If the model outputs a single score, we apply sigmoid for binary classification.
             # If the model outputs multiple scores, we apply softmax for multi-class classification.
-            y_pred = __convert_scores_to_probabilities(scores)
+            y_pred = convert_scores_to_probabilities(scores)
+            
+            # Save predictions and ground truth
+            predictions.append(y_pred.cpu().numpy())
+            ground_truth.append(y.cpu().numpy())
+
+            # Calculate the loss
+            loss = criterion(scores, y.float())
+            validation_loss_accum += loss.item()
+
+    # Convert predictions and ground truth to numpy arrays
+    predictions = np.concatenate(predictions, axis=0)
+    ground_truth = np.concatenate(ground_truth, axis=0)
+
+    # Convert predictions to binary using the threshold
+    predictions = (predictions >= ths).astype(int)
+
+    # Calculate the average validation loss
+    validation_loss = validation_loss_accum / len(dataloader)
+
+    # logging.info(f"Prediction probabilities | min: {predictions.min():.4f} | max: {predictions.max():.4f} | mean: {predictions.mean():.4f}")
+
+    # Calculate metrics
+    accuracy = metrics.accuracy_score(ground_truth, predictions)
+    f1_score = metrics.f1_score(ground_truth, predictions, zero_division=0)
+    precision = metrics.precision_score(ground_truth, predictions, zero_division=0)
+    recall = metrics.recall_score(ground_truth, predictions, zero_division=0)
+
+    # Save the results in a dictionary
+    result = {
+        'loss': validation_loss,
+        'accuracy': accuracy,
+        'f1_score': f1_score,
+        'precision': precision,
+        'recall': recall
+    }
+    
+    return result
+
+def multiclass_validation_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, criterion: torch.nn.Module, device: torch.device) -> float:
+    """
+    Performs a validation step on the model using the provided dataloader.
+
+    Args:
+        model (torch.nn.Module): PyTorch model to validate.
+        dataloader (torch.utils.data.DataLoader): Dataloader for the validation data.
+        criterion (torch.nn.Module): Criterion to compute the loss.
+        device (str): Device to run the validation on ('cpu' or 'cuda').
+
+    Returns:
+        float: Average validation loss.
+    """
+
+    # Save the predictions and ground truth labels
+    predictions = list()
+    ground_truth = list()
+
+    # Initialize the loss accumulator
+    validation_loss_accum = 0.0
+
+    for (X, y) in dataloader:
+        # Set the model to evaluation mode
+        model.eval()
+
+        # Start inference mode
+        with torch.inference_mode():
+            # Move data to the specified device
+            X, y = X.to(device), y.to(device)
+            
+            # Forward pass
+            scores = model(X)
+            scores = scores.squeeze()
+
+            # Convert scores to probabilities
+            # If the model outputs a single score, we apply sigmoid for binary classification.
+            # If the model outputs multiple scores, we apply softmax for multi-class classification.
+            y_pred = convert_scores_to_probabilities(scores)
+
+            # Convert predictions to class labels
+            # For multiclass classification, we take the argmax across the class dimension.
+            y_pred = torch.argmax(y_pred, dim=1)
             
             # Save predictions and ground truth
             predictions.append(y_pred.cpu().numpy())
@@ -72,11 +156,13 @@ def validation_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoa
     # Calculate the average validation loss
     validation_loss = validation_loss_accum / len(dataloader)
 
+    # logging.info(f"Prediction probabilities | min: {predictions.min():.4f} | max: {predictions.max():.4f} | mean: {predictions.mean():.4f}")
+
     # Calculate metrics
-    accuracy = binary_accuracy_score(ground_truth, predictions)
-    f1_score = binary_f1_score(ground_truth, predictions)
-    precision = binary_precision_score(ground_truth, predictions)
-    recall = binary_recall_score(ground_truth, predictions)
+    accuracy = metrics.accuracy_score(ground_truth, predictions)
+    f1_score = metrics.f1_score(ground_truth, predictions, average='weighted', zero_division=0)
+    precision = metrics.precision_score(ground_truth, predictions, average='weighted', zero_division=0)
+    recall = metrics.recall_score(ground_truth, predictions, average='weighted', zero_division=0)
 
     # Save the results in a dictionary
     result = {
